df1050ec654c8f58736419bbe31e3a723eb6222e
Blake Sherrill <bowhuntr11@hotmail.com>
2018-03-06 10:16:02 -0600

    Added MapsActivity and Location for Stands
    Added maps activity that passes latitude longitude to NewStand intent
    Added location button to NewStand and now saves latitude and longitude

---
+++ app/src/main/java/com/evilforge/bucktracker/MapsActivity.java
@@ -0,0 +1,100 @@
+package com.evilforge.bucktracker;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    Marker marker = null;
+    FloatingActionButton save_btn;
+
+    private LatLng standLoc;
+    private double latitude = 33;
+    private double longitude = -100;
+
+    String standName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("standName")){
+                standName = intent.getStringExtra("standName");
+            Log.d("NewStand", standName);
+        }
+
+
+        save_btn = findViewById(R.id.fab_save_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapsActivity.this, NewStand.class);
+                intent.putExtra("longitude", String.valueOf(longitude));
+                intent.putExtra("latitude", String.valueOf(latitude));
+                intent.putExtra("standName", standName);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng startLocation = new LatLng(latitude, longitude);
+        MarkerOptions options = new MarkerOptions()
+                                    .position(startLocation)
+                                    .draggable(true)
+                                    .title("Stand Location");
+
+
+        map.addMarker(options);
+        map.moveCamera(CameraUpdateFactory.newLatLng(startLocation));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 12));
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng arg0) {
+                if(marker != null) {
+                    marker.remove();
+                }
+                MarkerOptions markerOption = new MarkerOptions();
+                markerOption.position(arg0).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                marker = map.addMarker(markerOption);
+
+
+                LatLng position = marker.getPosition();
+                latitude = position.latitude;
+                longitude = position.longitude;
+
+                Log.d("Latitude", String.valueOf(latitude));
+                Log.d("Longitude", String.valueOf(longitude));
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/evilforge/bucktracker/NewStand.java
+++ app/src/main/java/com/evilforge/bucktracker/NewStand.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,7 +14,12 @@ import android.support.annotation.BinderThread;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -23,6 +29,16 @@ import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
@@ -43,7 +59,10 @@ import java.util.UUID;
 public class NewStand extends AppCompatActivity {
 
     EditText standName;
+    Button location;
     Button saveStand;
+    String latitude = "null";
+    String longitude = "null";
 
 
     @Override
@@ -52,7 +71,40 @@ public class NewStand extends AppCompatActivity {
         setContentView(R.layout.activity_new_stand);
 
         standName = findViewById(R.id.stand_name);
+        location = findViewById(R.id.select_location_button);
         saveStand = findViewById(R.id.saveNewStand);
+
+        Log.d("NewStand", "intent started");
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("standName")){
+            String standNames = intent.getStringExtra("standName");
+            Log.d("NewStand", standNames);
+            standName.setText(standNames);
+            standName.invalidate();
+        }
+        if(intent.hasExtra("latitude")){
+            latitude = intent.getStringExtra("latitude");
+            Log.d("NewStand", latitude);
+        }
+        if(intent.hasExtra("longitude")){
+            longitude = intent.getStringExtra("longitude");
+            Log.d("NewStand", longitude);
+        }
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewStand.this, MapsActivity.class);
+                String standNames = standName.getText().toString();
+                if (!standNames.matches("")) {
+                    intent.putExtra("standName", standNames);
+                }
+                finish();
+                startActivity(intent);
+            }
+        });
+
 
         saveStand.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,16 +112,19 @@ public class NewStand extends AppCompatActivity {
                 saveStand();
             }
         });
+
     }
 
     private void saveStand() {
         String standNames = standName.getText().toString();
-        String latitude = "null";
-        String longitude = "null";
-        FirebaseDatabase.getInstance().getReference("stands")
-                .child(standNames)
-                .setValue(new Stands(standNames, latitude, longitude));
-        finish();
+        if (!standNames.equalsIgnoreCase("") || latitude.equalsIgnoreCase("null") || longitude.equalsIgnoreCase("null")) {
+            FirebaseDatabase.getInstance().getReference("stands")
+                    .child(standNames)
+                    .setValue(new Stands(standNames, latitude, longitude));
+            finish();
+        } else {
+            Toast.makeText(NewStand.this, "Fill out all information", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
